5859de1ff8c2ace8bb8079e3043ebf888a70961d
Quang Lam <laulua1223@outlook.com>
2025-12-12 12:15:52 +0700

    create project structure

---
+++ src/test/java/hcmute/edu/vn/discord/DiscordCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package hcmute.edu.vn.discord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscordCloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
