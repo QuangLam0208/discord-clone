418849325186761bdc8efeb458d797dd773fd2a6
Quang Lam <laulua1223@outlook.com>
2025-12-14 22:15:41 +0700

    update repo

--- src/main/java/hcmute/edu/vn/discord/repository/ServerRepository.java
+++ src/main/java/hcmute/edu/vn/discord/repository/ServerRepository.java
@@ -1,4 +1,12 @@
 package hcmute.edu.vn.discord.repository;
 
-public interface ServerRepository {
+import hcmute.edu.vn.discord.entity.jpa.Server;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ServerRepository extends JpaRepository<Server, Long> {
+    List<Server> findByOwner_Id(Long ownerId);
 }
--- src/main/java/hcmute/edu/vn/discord/repository/UserRepository.java
+++ src/main/java/hcmute/edu/vn/discord/repository/UserRepository.java
@@ -1,4 +1,14 @@
 package hcmute.edu.vn.discord.repository;
 
-public interface UserRepository {
+import hcmute.edu.vn.discord.entity.jpa.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
+    Boolean existsByUsername(String username);
+    Boolean existsByEmail(String email);
 }
