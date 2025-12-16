3e40d346f6f1e35d5a3dac75c19d4c98db039bf8
Quang Lam <laulua1223@outlook.com>
2025-12-16 13:34:39 +0700

    add repo for server structure

---
+++ src/main/java/hcmute/edu/vn/discord/repository/InviteRepository.java
@@ -0,0 +1,17 @@
+package hcmute.edu.vn.discord.repository;
+
+import hcmute.edu.vn.discord.entity.jpa.Invite;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface InviteRepository extends JpaRepository<Invite, Long> {
+    // Tìm invite theo mã code (để user join server)
+    Optional<Invite> findByCode(String code);
+
+    // Lấy danh sách invite của server (cho admin quản lý)
+    List<Invite> findByServerId(Long serverId);
+}
