fd5fa17d4e5289362c00c034f45f01bd15358b1c
Quang Lam <laulua1223@outlook.com>
2025-12-16 13:45:48 +0700

    add repo for admin permission

--- src/main/java/hcmute/edu/vn/discord/repository/PermissionRepository.java
+++ src/main/java/hcmute/edu/vn/discord/repository/PermissionRepository.java
@@ -1,4 +1,13 @@
 package hcmute.edu.vn.discord.repository;
 
-public interface PermissionRepository {
+import hcmute.edu.vn.discord.entity.jpa.Permission;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PermissionRepository extends JpaRepository<Permission, Long> {
+    // Tìm quyền theo mã code (VD: MESSAGE_DELETE)
+    Optional<Permission> findByCode(String code);
 }
--- src/main/java/hcmute/edu/vn/discord/repository/SubscriptionRepository.java
+++ src/main/java/hcmute/edu/vn/discord/repository/SubscriptionRepository.java
@@ -1,4 +1,14 @@
 package hcmute.edu.vn.discord.repository;
 
-public interface SubscriptionRepository {
+import hcmute.edu.vn.discord.entity.jpa.Subscription;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Date;
+import java.util.Optional;
+
+@Repository
+public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {
+    // Kiểm tra user có đang đăng ký gói nào còn hạn không
+    Optional<Subscription> findByUserIdAndEndDateAfter(Long userId, Date now);
 }
