f0ef5ff392b62003054241db2d1a5dba323b4e52
Lam Khanh Duy <23110084@student.hcmute.edu.vn>
2025-12-17 15:12:13 +0700

    Create Repository and Service of UserSecurity, DirectMessage and Friend

--- src/main/java/hcmute/edu/vn/discord/repository/FriendRepository.java
+++ src/main/java/hcmute/edu/vn/discord/repository/FriendRepository.java
@@ -2,7 +2,10 @@ package hcmute.edu.vn.discord.repository;
 
 import hcmute.edu.vn.discord.entity.enums.FriendStatus;
 import hcmute.edu.vn.discord.entity.jpa.Friend;
+import hcmute.edu.vn.discord.entity.jpa.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,10 +13,34 @@ import java.util.Optional;
 
 @Repository
 public interface FriendRepository extends JpaRepository<Friend, Long> {
-    // Tìm danh sách bạn bè (đã accept) của 1 user (cần check cả 2 chiều requester/receiver)
-    List<Friend> findByRequesterIdAndStatus(Long requesterId, FriendStatus status);
-    List<Friend> findByReceiverIdAndStatus(Long receiverId, FriendStatus status);
+    // Lấy lời mời kết bạn mà User đã gửi (PENDING)
+    List<Friend> findByRequesterIdAndStatus(
+            Long requesterId,
+            FriendStatus status
+    );
 
-    // Kiểm tra quan hệ giữa 2 người (để chặn spam request)
-    Optional<Friend> findByRequesterIdAndReceiverId(Long requesterId, Long receiverId);
+    // Lấy Lời mời kết bạn mà User đã nhận (PENDING)
+    List<Friend> findByReceiverIdAndStatus(
+            Long receiverId,
+            FriendStatus status
+    );
+
+    // Tìm mối quan hệ giữa 2 user theo 2 chiều
+    Optional<Friend> findByRequesterAndReceiverOrRequesterAndReceiver(
+            User requester1, User receiver1,
+            User requester2, User receiver2
+    );
+
+    // Lấy danh sách bạn bè đã ACCEPT
+    @Query("""
+            SELECT f FROM Friend f
+            WHERE f.status = :status
+                  AND (f.requester.id = :userId OR f.receiver.id = :userId)
+            """)
+    List<Friend> findAllAcceptedFriendsOfUser(
+            @Param("userId") Long userId,
+            @Param("status") FriendStatus status
+    );
+
+    Optional<Friend> findByRequesterAndReceiver(User requester, User receiver);
 }
