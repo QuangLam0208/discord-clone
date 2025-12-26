f535107e5dd91703c6af01ac832808d3a1361e25
Quang Lam <laulua1223@outlook.com>
2025-12-27 00:00:30 +0700

    add description helper

--- src/main/java/hcmute/edu/vn/discord/entity/enums/EPermission.java
+++ src/main/java/hcmute/edu/vn/discord/entity/enums/EPermission.java
@@ -1,20 +1,30 @@
 package hcmute.edu.vn.discord.entity.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum EPermission {
-    // Nhóm General
-    ADMIN,
-    VIEW_CHANNELS,
-    MANAGE_SERVER,
+    // General
+    ADMIN("Full access"),
+    VIEW_CHANNELS("View channels"),
+    MANAGE_SERVER("Manage server settings"),
 
-    // Nhóm Channel
-    MANAGE_CHANNELS,
-    SEND_MESSAGES,
+    // Channel
+    MANAGE_CHANNELS("Create/update/delete channels"),
+    SEND_MESSAGES("Send messages"),
 
-    // Nhóm Moderation
-    KICK_MEMBERS,
-    BAN_MEMBERS;
+    // Moderation
+    KICK_MEMBERS("Kick members"),
+    BAN_MEMBERS("Ban members");
+
+    private final String description;
+
+    EPermission(String description) {
+        this.description = description;
+    }
 
     public String getCode() {
         return name();
     }
+
 }
--- src/main/java/hcmute/edu/vn/discord/entity/jpa/Permission.java
+++ src/main/java/hcmute/edu/vn/discord/entity/jpa/Permission.java
@@ -1,18 +1,22 @@
 package hcmute.edu.vn.discord.entity.jpa;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "permissions")
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Permission {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(unique = true, nullable = false, updatable = false)
-    private String code; // Ví dụ: SERVER_MANAGE, MESSAGE_DELETE
+    private String code; // e.g., SERVER_MANAGE, MESSAGE_DELETE
 
     private String description;
 }
