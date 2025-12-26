3f340135b0ac43e26ecf416cf9d801a9330175d6
Quang Lam <laulua1223@outlook.com>
2025-12-26 13:23:52 +0700

    add descr

--- src/main/java/hcmute/edu/vn/discord/entity/jpa/Permission.java
+++ src/main/java/hcmute/edu/vn/discord/entity/jpa/Permission.java
@@ -1,27 +1,18 @@
 package hcmute.edu.vn.discord.entity.jpa;
 
 import jakarta.persistence.*;
-import lombok.*;
-import hcmute.edu.vn.discord.entity.enums.EPermission;
+import lombok.Data;
 
 @Entity
 @Table(name = "permissions")
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class Permission {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String name;
+    @Column(unique = true, nullable = false, updatable = false)
+    private String code; // Ví dụ: SERVER_MANAGE, MESSAGE_DELETE
 
-    @Column(unique = true, nullable = false)
-    private String code;
-
-    public Permission(String name, EPermission permissionEnum) {
-        this.name = name;
-        this.code = permissionEnum.name(); // Lấy tên Enum lưu vào String (VD: "MANAGE_CHANNELS")
-    }
+    private String description;
 }
