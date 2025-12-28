9a4b54df6fa310d26e03423e1de3ca1781db5292
Luong Quang Lam <lamlq28@gmail.com>
2025-12-28 23:07:33 +0700

    Remove EnableJpaRepositories annotation
    Removed the EnableJpaRepositories annotation.

--- src/main/java/hcmute/edu/vn/discord/DiscordCloneApplication.java
+++ src/main/java/hcmute/edu/vn/discord/DiscordCloneApplication.java
@@ -2,10 +2,8 @@ package hcmute.edu.vn.discord;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "hcmute.edu.vn.discord.repository")
 public class DiscordCloneApplication {
 
     public static void main(String[] args) {
