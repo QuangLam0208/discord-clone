977e7a4f12dbe75613f9988132f0544998adf1b9
Quang Lam <laulua1223@outlook.com>
2026-01-03 09:00:49 +0700

    update

--- src/main/java/hcmute/edu/vn/discord/repository/AuditLogRepository.java
+++ src/main/java/hcmute/edu/vn/discord/repository/AuditLogRepository.java
@@ -1,4 +1,17 @@
 package hcmute.edu.vn.discord.repository;
 
-public interface AuditLogRepository {
+import hcmute.edu.vn.discord.entity.mongo.AuditLog;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface AuditLogRepository extends MongoRepository<AuditLog, String> {
+
+    Page<AuditLog> findByActionContainingIgnoreCaseAndDetailContainingIgnoreCase(
+            String action, String keyword, Pageable pageable
+    );
+
+    Page<AuditLog> findByAdminIdAndActionContainingIgnoreCaseAndDetailContainingIgnoreCase(
+            Long adminId, String action, String keyword, Pageable pageable
+    );
 }
